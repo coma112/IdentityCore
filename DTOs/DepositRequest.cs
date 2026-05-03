0b4b82e0d9ddd1cca94c1ffbb51fc2b7e69e90c7
coma112 <lolkd005@gmail.com>
2026-05-03 17:21:54 +0200

    refactor(dto): Implemented new minimum decimal attribute.

--- DTOs/DepositRequest.cs
+++ DTOs/DepositRequest.cs
@@ -1,12 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using IdentityCore.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace IdentityCore.DTOs
 {
     public class DepositRequest
     {
         [Required]
-        [Range(typeof(decimal), "10", "79228162514264337593543950335",
-        ErrorMessage = "Minimum deposit amount is 10.")]
+        [MinDecimal(10, ErrorMessage = "Minimum deposit amount is 10.")]
         public decimal Amount { get; set; }
     }
 }
--- DTOs/WithdrawRequest.cs
+++ DTOs/WithdrawRequest.cs
@@ -1,12 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using IdentityCore.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace IdentityCore.DTOs
 {
     public class WithdrawRequest
     {
         [Required]
-        [Range(typeof(decimal), "0.01", "79228162514264337593543950335",
-        ErrorMessage = "Amount must be greater than 0.")]
+        [MinDecimal(0.01, ErrorMessage = "Amount must be greater than 0.")]
         public decimal Amount { get; set; }
     }
 }

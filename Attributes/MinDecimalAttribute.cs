3c3965880dafbd6d7fd661d07e1591f8c2ad65d4
coma112 <lolkd005@gmail.com>
2026-05-03 17:21:02 +0200

    feat(attr): Found new solution for [Range]'s decimal problem.

---
+++ Attributes/MinDecimalAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityCore.Attributes
+{
+    public class MinDecimalAttribute : ValidationAttribute
+    {
+        private readonly decimal _minimum;
+
+        public MinDecimalAttribute(double minimum)
+        {
+            _minimum = (decimal)minimum;
+            ErrorMessage = $"Amount must be at least {_minimum}.";
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is decimal amount && amount >= _minimum)
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage);
+        }
+    }
+}
